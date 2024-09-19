ecf2c71a456e2f91f8d291c6e18ba47421904ce1
Ventura <ventura.py3@gmail.com>
2024-09-19 19:29:48 -0300

    Modifica o CSS

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const sim = document.getElementById("opcyes");
-const nao = document.getElementById("opcno");
-const fundo = document.getElementById("area");
-const resp = document.getElementById("textinho");
+const sim = document.querySelector("#option-yes");
+const nao = document.querySelector("#option-no");
+const fundo = document.querySelector("#area");
+const resp = document.querySelector("#textinho");
 
 nao.addEventListener("mouseenter", entrar);
 let estado = -1;
 
 function entrar() {
   estado++;
-  if (estado == 0) {
-    sim.style.transform = "translate(95px)";
-    nao.style.transform = "translate(130px, -150px)";
-  }
-  if (estado == 1) {
-    nao.style.transform = "translate(130px, 150px)";
-  }
-  if (estado == 2) {
-    nao.style.transform = "translate(-325px, 150px)";
-  }
-  if (estado == 3) {
-    nao.style.transform = "translate(-325px, -150px)";
-  }
-  if (estado == 4) {
-    nao.style.transform = "translate(135px, 150px)";
-  }
-  if (estado == 5) {
-    nao.style.transform = "translate(135px, -150px)";
-  }
-  if (estado == 6) {
-    nao.style.transform = "translate(-325px, 150px)";
-  }
-  if (estado == 7) {
-    nao.style.transform = "translate(-325px, -150px)";
-    estado = -1;
+  switch (estado) {
+    case 0:
+      sim.style.transform = "translate(95px)";
+      nao.style.transform = "translate(130px, -150px)";
+      break;
+    case 1:
+      nao.style.transform = "translate(130px, 150px)";
+      break;
+    case 2:
+      nao.style.transform = "translate(-325px, 150px)";
+      break;
+    case 3:
+      nao.style.transform = "translate(-325px, -150px)";
+      break;
+    case 4:
+      nao.style.transform = "translate(135px, 150px)";
+      break;
+    case 5:
+      nao.style.transform = "translate(135px, -150px)";
+      break;
+    case 6:
+      nao.style.transform = "translate(-325px, 150px)";
+      break;
+    case 7:
+      nao.style.transform = "translate(-325px, -150px)";
+      estado = -1;
+      break;
   }
 }
 
@@ -40,6 +42,7 @@ sim.addEventListener("click", clicar);
 
 function clicar() {
   resp.innerHTML = "Você caiu no golpe do colonizador";
+  
   sim.style.transform = "translate(0px)";
   nao.style.transform = "translate(0px, 0px)";
 
